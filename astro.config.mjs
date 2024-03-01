120f03f8ddf45c44672af0b0ce2fd5a0166fcf3f
Cristian Cassetta <cristiancassetta@gmail.com>
2024-03-01 21:47:34 +0100

    First Commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    viewTransitions: true,
+    assets: true,
+  },
+  integrations: [tailwind(), compress()],
+});
